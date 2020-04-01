a1caceea12db5507259ad2cf21541e92a82cf1c4
Softkeys <azeezlukman95@gmail.com>
2020-04-01 15:59:02 +0100

    Created map component

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,10 @@
+import { MapComponent } from './map/map.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+    declarations: [MapComponent],
+    imports: [],
+    exports: [MapComponent]
+})
+
+export class ComponentsModule { }
--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    ComponentsModule,
     FolderPageRoutingModule
   ],
   declarations: [FolderPage]
