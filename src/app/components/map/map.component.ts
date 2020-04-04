ccc1d1cfad00799e1708f6cc124bbd294eb5680b
Softkeys <azeezlukman95@gmail.com>
2020-04-04 18:07:45 +0100

    Add zoom and rotation controls to the map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -27,7 +27,10 @@ export class MapComponent implements OnInit, AfterViewInit {
     }).on('dataloading', () => {
       window.dispatchEvent(new Event('resize'));
       // mapboxObj.resize(); also work
-    });
+    })
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());;
   }
 
   constructor(private router: Router, private dataService: ApiDataService, private popupService: PopupService) { }
