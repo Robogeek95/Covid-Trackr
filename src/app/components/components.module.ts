c3110039ee593fe0bfda6bc7168a730808ed5ade
Softkeys <azeezlukman95@gmail.com>
2020-05-07 11:17:36 +0100

    updated navbar

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,13 @@
 import { IonicModule } from '@ionic/angular';
 import { MapComponent } from './map/map.component';
 import { NgModule } from '@angular/core';
+import { ColorCodeComponent } from './color-code/color-code.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-    declarations: [MapComponent],
-    imports: [IonicModule],
-    exports: [MapComponent]
+    declarations: [MapComponent, ColorCodeComponent],
+    imports: [IonicModule, CommonModule],
+    exports: [MapComponent, ColorCodeComponent]
 })
 
 export class ComponentsModule { }
